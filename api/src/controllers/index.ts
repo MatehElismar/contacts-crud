0f18d20c8548001abd1e89a053efb686909f8058
Mateh Elismar <matematicoelismar@gmail.com>
2022-12-13 22:48:01 -0400

    update api focus

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,1 +1,1 @@
-export * from "./contact.ctrl";
+export * from "./student.ctrl";
--- api/src/models/contact.model.ts
+++
@@ -1,9 +0,0 @@
-import { createSchema, Type, typedModel } from "ts-mongoose";
-
-const ContactSchema = createSchema({
-  fullName: Type.string({ required: true }),
-  phoneNumber: Type.string({ required: true }),
-  email: Type.string({ required: false }),
-});
-
-export const Contact = typedModel("Contact", ContactSchema);
